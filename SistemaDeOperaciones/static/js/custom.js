52a905d3a179248db86a7910cc60fedb4fd2fd3c
Pduhart <pduhart@icpacifico.cl>
2024-04-18 16:58:47 -0400

    DataTables y Bug Fixes

--- SistemaDeOperaciones/static/js/custom.js
+++ SistemaDeOperaciones/static/js/custom.js
@@ -1,1 +1,14 @@
-new DataTable('#example-table-bd',{language: {url: '//cdn.datatables.net/plug-ins/2.0.4/i18n/es-ES.json',},});
+new DataTable('#example-table-bd',{
+    language: {
+        paginate: {
+            first: '<span class="mdi mdi-chevron-double-left"></span>',
+            last: '<span class="mdi mdi-chevron-double-right"></span>',
+            next: '<span class="mdi mdi-chevron-right"></span>',
+            previous: '<span class="mdi mdi-chevron-left"></span>'
+        },
+        url: '//cdn.datatables.net/plug-ins/2.0.4/i18n/es-CL.json',      
+    },
+    scrollX: true,
+    scrollY: '50vh',
+    pagingType: 'simple_numbers',
+});
