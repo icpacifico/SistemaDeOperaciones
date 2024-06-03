c368c30601f89a2a98ef9cd231e1cb04b321b74b
Pduhart <pduhart@icpacifico.cl>
2024-06-03 13:20:06 -0400

    Update views.py

--- SistemaDeOperaciones/Ventas/views.py
+++ SistemaDeOperaciones/Ventas/views.py
@@ -97,6 +97,7 @@ class CrearCotizacion(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['clientes'] = Cliente.objects.all()
+        context['condominios'] = Condominio.objects.values_list('id_condominio','nombre_condominio')
         return context
 
 
